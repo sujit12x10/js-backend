85c066d2e73f9930d884b49f1680cddd6236ed63
sujit12x10 <sujit12x10@gmail.com>
2025-03-10 10:58:42 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+/*
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on post ${process.env.PORT}`)
+        })
+
+    } catch(error){
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+*/
